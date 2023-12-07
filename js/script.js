ad648afecd1aebdd5c1831bccf077811ccc5a57b
Bruno César <brunocesar.tenorio1@gmail.com>
2023-12-07 17:55:16 -0300

    fixing input

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ const cep = document.querySelector("[data-cep]");
 const button = document.querySelector("[data-button]");
 const adress = document.querySelector("[data-adress]");
 if (cep && button && adress) {
-    button.addEventListener("click", showAdress);
+  button.addEventListener("click", showAdress);
+
   function showAdress(event) {
     event.preventDefault();
-    const cepValue = cep.value;
-    activeAdress(cepValue);
+    activeAdress(cep.value);
   }
   function activeAdress(cep) {
     fetch(`https://viacep.com.br/ws/${cep}/json`)
@@ -15,5 +15,9 @@ if (cep && button && adress) {
         adress.innerText = body;
         console.log(body);
       });
+  }
+  cep.addEventListener("input", removeNumbers);
+  function removeNumbers() {
+    cep.value = cep.value.replace(/\D/, "");
   }
 }
