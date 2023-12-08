b5ffe257fd4a58cdc8a44b5638cca785f24303a2
Bruno César <brunocesar.tenorio1@gmail.com>
2023-12-08 08:51:42 -0300

    updating project

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-const cep = document.querySelector("[data-cep]");
+const cep = document.getElementById("cep");
 const button = document.querySelector("[data-button]");
 const adress = document.querySelector("[data-adress]");
 if (cep && button && adress) {
-  button.addEventListener("click", showAdress);
-
-  function showAdress(event) {
+  button.addEventListener("click", handleButton);
+  function handleButton(event) {
     event.preventDefault();
-    activeAdress(cep.value);
+    showAdress(cep.value);
   }
-  function activeAdress(cep) {
+  function showAdress(cep) {
     fetch(`https://viacep.com.br/ws/${cep}/json`)
-      .then((response) => response.text())
-      .then((body) => {
-        adress.innerText = body;
-        console.log(body);
+      .then((response) => response.json())
+      .then((data) => {
+        const dataFormatted = `CEP: ${data.cep}\n Logradouro: ${data.logradouro}\n Bairro: ${data.bairro}\n Complemento: ${data.complemento}\n Localidade: ${data.localidade}\n UF: ${data.uf}\n DDD: ${data.ddd}\n `;
+        adress.innerText = dataFormatted;
       });
   }
-  cep.addEventListener("input", removeNumbers);
-  function removeNumbers() {
+  cep.addEventListener("input", removeNoNumbers);
+  function removeNoNumbers(event) {
     cep.value = cep.value.replace(/\D/, "");
   }
 }
