5399ce33b5cbd0a2ebb1356418bab89dc397f087
Bruno César <brunocesar.tenorio1@gmail.com>
2023-12-08 12:56:13 -0300

    updating js

--- js/script.js
+++ js/script.js
@@ -2,27 +2,25 @@ const cep = document.getElementById("cep");
 const button = document.querySelector("[data-button]");
 const adress = document.querySelector("[data-adress]");
 if (cep && button && adress) {
-  button.addEventListener("click", handleClick);
-  function handleClick(event) {
-    event.preventDefault();
-    showAdress(cep.value);
-  }
-  function showAdress(cep) {
-    fetch(`https://viacep.com.br/ws/${cep}/json`)
-      .then((response) => response.json())
-      .then((data) => {
-        const errorMessage =
-          "CEP não encontrado, verifique e insira novamente.";
-        const dataFormatted = `CEP: ${data.cep}\n Logradouro: ${data.logradouro}\n Bairro: ${data.bairro}\n Complemento: ${data.complemento}\n Localidade: ${data.localidade}\n UF: ${data.uf}\n DDD: ${data.ddd}\n`;
-        if (data.erro) {
-          adress.innerText = errorMessage;
-        } else {
-          adress.innerText = dataFormatted;
-        }
-      });
-  }
+  const errorMessage = "CEP não encontrado, verifique e digite novamente";
+  const errorMessage2 = "CEP inválido, insira pelo menos 8 dígitos";
   cep.addEventListener("input", removeNonNumbers);
+  button.addEventListener("click", CEPIsValid);
   function removeNonNumbers() {
     cep.value = cep.value.replace(/\D/, "");
+  }
+  function CEPIsValid(event) {
+    event.preventDefault();
+    return /^\d{8}$/.test(cep.value)
+      ? showAdress(cep.value)
+      : (adress.innerText = errorMessage2);
+  }
+  function showAdress(value) {
+    fetch(`https://viacep.com.br/ws/${value}/json`)
+    .then(response => response.json())
+    .then((data) => {
+      const dataFormatted = `CEP: ${data.cep}\n Logradouro: ${data.logradouro}\n Bairro: ${data.bairro}\n Complemento: ${data.complemento}\n Localidade: ${data.localidade}\n UF: ${data.uf}\n DDD: ${data.ddd}\n`
+      return data.erro ? adress.innerText = errorMessage : adress.innerText = dataFormatted;
+    })
   }
 }
