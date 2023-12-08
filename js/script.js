d31f08b454177b743a9177bee9ef3c9cd61346fd
Bruno César <brunocesar.tenorio1@gmail.com>
2023-12-08 09:27:05 -0300

    adding error message

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const cep = document.getElementById("cep");
 const button = document.querySelector("[data-button]");
 const adress = document.querySelector("[data-adress]");
 if (cep && button && adress) {
-  button.addEventListener("click", handleButton);
-  function handleButton(event) {
+  button.addEventListener("click", handleClick);
+  function handleClick(event) {
     event.preventDefault();
     showAdress(cep.value);
   }
@@ -11,12 +11,18 @@ if (cep && button && adress) {
     fetch(`https://viacep.com.br/ws/${cep}/json`)
       .then((response) => response.json())
       .then((data) => {
-        const dataFormatted = `CEP: ${data.cep}\n Logradouro: ${data.logradouro}\n Bairro: ${data.bairro}\n Complemento: ${data.complemento}\n Localidade: ${data.localidade}\n UF: ${data.uf}\n DDD: ${data.ddd}\n `;
-        adress.innerText = dataFormatted;
+        const errorMessage =
+          "CEP não encontrado, verifique e insira novamente.";
+        const dataFormatted = `CEP: ${data.cep}\n Logradouro: ${data.logradouro}\n Bairro: ${data.bairro}\n Complemento: ${data.complemento}\n Localidade: ${data.localidade}\n UF: ${data.uf}\n DDD: ${data.ddd}\n`;
+        if (data.erro) {
+          adress.innerText = errorMessage;
+        } else {
+          adress.innerText = dataFormatted;
+        }
       });
   }
-  cep.addEventListener("input", removeNoNumbers);
-  function removeNoNumbers(event) {
+  cep.addEventListener("input", removeNonNumbers);
+  function removeNonNumbers() {
     cep.value = cep.value.replace(/\D/, "");
   }
 }
