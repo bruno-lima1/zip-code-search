3d138423c85ed4db60c66da0090b8acf7e53ab75
Bruno César <brunocesar.tenorio1@gmail.com>
2024-01-23 10:48:46 -0300

    updating js

--- js/script.js
+++ js/script.js
@@ -2,23 +2,23 @@ const cep = document.getElementById("cep");
 const button = document.querySelector("[data-button]");
 const adress = document.querySelector("[data-adress]");
 if (cep && button && adress) {
+  button.addEventListener("click", cepIsValid);
   cep.addEventListener("input", removeNonNumbers);
-  button.addEventListener("click", CEPIsValid);
-  function removeNonNumbers() {
-    return (cep.value = cep.value.replace(/\D/, ""));
-  }
-  function CEPIsValid(event) {
+  function cepIsValid(event) {
     event.preventDefault();
     return /^\d{8}$/.test(cep.value)
       ? showAdress(cep.value)
-      : (adress.innerText = "CEP inválido, insira pelo menos 8 dígitos");
+      : (adress.innerHTML = "CEP inválido, insira pelo menos 8 caracteres");
   }
-  async function showAdress(value) {
-    const response = await fetch(`https://viacep.com.br/ws/${value}/json`);
+  function removeNonNumbers() {
+    return (cep.value = cep.value.replace(/\D/, ""));
+  }
+  async function showAdress(cep) {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json`);
     const data = await response.json();
-    const dataFormatted = `CEP: ${data.cep}\n Logradouro: ${data.logradouro}\n Bairro: ${data.bairro}\n Complemento: ${data.complemento}\n Cidade: ${data.localidade}\n UF: ${data.uf}\n DDD: ${data.ddd}\n`;
+    const dataFormatted = `CEP: ${data.cep}\n Logradouro: ${data.logradouro}\n Bairro: ${data.bairro}\n Complemento: ${data.complemento}\n Cidade: ${data.localidade}\n UF: ${data.uf}\n DDD: ${data.ddd}`;
     return data.erro
-      ? (adress.innerText = "Esse CEP não existe, verifique e digite novamente")
+      ? (adress.innerHTML = "Esse CEP não existe, verifique e digite novamente")
       : (adress.innerText = dataFormatted);
   }
 }
