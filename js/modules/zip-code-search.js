f74592e8954efae85c7f3698adb9ac8051ddcc7e
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-25 14:38:18 -0300

    updating js

--- js/modules/zip-code-search.js
+++ js/modules/zip-code-search.js
@@ -4,11 +4,11 @@ export default function zipCodeSearch(input, search, message) {
   const adress = document.querySelector(message);
 
   const removeNonNumbers = () => {
-    return (cep.value = cep.value.replace(/[^\d-]/gi, ""));
+    return cep.value = cep.value.replace(/[^\d-]/gi, "")
   }
   const cepIsValid = (event) => {
     event.preventDefault();
-    return /(\d{5})-?(\d{3})/.test(cep.value)
+    return /(\d{5})-?(\d{3})/gi.test(cep.value)
     ? showAdress(cep.value)
     : (adress.innerHTML = "CEP inválido, verifique e digite novamente") && cep.classList.add("error");
   }
@@ -18,15 +18,11 @@ export default function zipCodeSearch(input, search, message) {
   }
   const showAdress = async (cepValue) => {
     try {
-      const response = await fetch(
-        `https://viacep.com.br/ws/${cepValue}/json`
-      );
+      const response = await fetch(`https://viacep.com.br/ws/${cepValue}/json`);
       const data = await response.json();
       const dataFormatted = `CEP: ${data.cep}\n Logradouro: ${data.logradouro}\n Bairro: ${data.bairro}\n Complemento: ${data.complemento}\n Cidade: ${data.localidade}\n UF: ${data.uf}\n DDD: ${data.ddd}`;
       return data.erro
-        ? (adress.innerHTML =
-            "Esse CEP não existe, verifique e digite novamente") &&
-            cep.classList.add("error")
+        ? (adress.innerHTML = "Esse CEP não existe, verifique e digite novamente") && cep.classList.add("error")
         : (adress.innerText = dataFormatted) && cep.classList.remove("error");
     } catch (erro) {
       console.log(erro);
