45490f495910a94d190b784ff4248a0543a96639
Bruno César <brunocesar.tenorio1@gmail.com>
2024-05-20 16:15:36 -0300

    updating css input

--- js/modules/zip-code-search.js
+++ js/modules/zip-code-search.js
@@ -19,8 +19,8 @@ export default function zipCodeSearch(input, search, message) {
   const returnCEP = (event) => {
     event.preventDefault();
     return validation()
-    ? (cep.value = build(clear(cep.value)), cep.classList.remove("error"), showAdress())
-    : (cep.classList.add("error"), adress.innerHTML = "CEP inválido, verifique e digite novamente");
+    ? (cep.value = build(clear(cep.value)), cep.classList.remove("error"), cep.classList.add("valid"), showAdress())
+    : (cep.classList.add("error"), cep.classList.remove("valid"), adress.innerHTML = "CEP inválido, verifique e digite novamente");
   }
   if (cep && button && adress) {
     cep.addEventListener("input", removeNonNumbers);
--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 import zipCodeSearch from "./modules/zip-code-search.js";
 zipCodeSearch("cep", "[data-button]", "[data-adress]");
+
+
+
